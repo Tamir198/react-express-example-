1a8403964227765b447e3690d3162f0dd608df3e
Tamir <1tamir198@gmail.com>
2021-10-01 17:38:50 +0300

    Adding example for get request

--- client-react/src/Components/someComponenet.js
+++ client-react/src/Components/someComponenet.js
@@ -1,5 +1,17 @@
-const SomeComponent = () =>{
-  return <button>I am buttom from another file</button>
+
+const SomeComponent = () => {
+
+  const getData = async  () => {
+    fetch("/", {
+      method: "GET",
+      mode: 'no-cors'
+      //TODO print the response message
+    }).then(response => response.json()).then(data => console.log(data));
+  }
+
+  return <button onClick={getData}>I am buttom from another file</button>
 }
+
+
 
 export default SomeComponent;
